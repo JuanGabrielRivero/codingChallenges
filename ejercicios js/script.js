a17a7d1ad3e555aae2183895c2d616e6a580e10b
Juani <jrm3301@gmail.com>
2022-07-25 13:47:27 +0100

    ejercicio 3 logical operators && else if

--- ejercicios js/script.js
+++ ejercicios js/script.js
@@ -1,14 +1,16 @@
 // Ejercicios
+// -------------------------------------------EJERCICIO 1------------------------------------------------------------------
 
 // LECTURE: Values and Variables
 // 1. Declare variables called 'country', 'continent' and 'population' and
 // assign their values according to your own country (population in millions)
 // 2. Log their values to the console
 
-
 /* let country = "Spain";
  let continent = "Europe";
  let population = 47; */
+
+ // -------------------------------------------EJERCICIO 2------------------------------------------------------------------
 
 // LECTURE: Data Types
 // 1. Declare a variable called 'isIsland' and set its value according to your
@@ -25,6 +27,9 @@
  console.log(typeof country);
  console.log(typeof language); */
 
+ // -------------------------------------------EJERCICIO 3------------------------------------------------------------------
+
+
 // LECTURE: let, const and var
 // 1. Set the value of 'language' to the language spoken where you live (some
 // countries have multiple languages, but just choose one)
@@ -37,6 +42,8 @@
  const continent = "Europe";
  const isIsland = false;
  isIsland = true; */
+
+// -------------------------------------------EJERCICIO 4------------------------------------------------------------------
 
 
 // LECTURE: Basic Operators
@@ -62,6 +69,7 @@
  console.log(description1); */
 
 
+// -------------------------------------------EJERCICIO 5------------------------------------------------------------------
 
 // LECTURE: Strings and Template Literals
 //  1. Recreate the 'description' variable from the last assignment, this time
@@ -77,14 +85,14 @@ const description = `${country} is in ${continent} and it's ${population} millio
 
  console.log(description); */
 
+// -------------------------------------------EJERCICIO 6------------------------------------------------------------------
 
 //  LECTURE: Taking Decisions: if / else Statements
 // 1. If your country's population is greater that 33 million, log a string like this to the
 // console: 'Portugal's population is above average'. Otherwise, log a string like
 // 'Portugal's population is 22 million below average' (the 22 is the average of 33
 // minus the country's population)
-// 2. After checking the result, change the population temporarily to 13 and then to
-// 130. See the different results, and set the population back to original
+
 
 /*
 const population = 47;
@@ -95,6 +103,8 @@ if(population > 33){
 }else{
     console.log(`${country}'s population is ${33 - population} million below average`);
 }
+ -------------------------------------------EJERCICIO 7------------------------------------------------------------------
+
 */
 // LECTURE: Type Conversion and Coercion
 // 1. Predict the result of these 5 operations without executing them:
@@ -105,21 +115,52 @@ if(population > 33){
 // 5 + 6 + '4' + 9 - 4 - 2;
 // 2. Execute the operations to check if you were right
 
-//1º
-4
-console.log("9" - "5");
+// 4
+// console.log("9" - "5");
 
-617
+// 617
 
-console.log('19' - '13' + '17');
+// console.log('19' - '13' + '17');
 
-617
+// 617
 
-console.log('19' - '13' + 17); 23
+// console.log('19' - '13' + 17); 23
 
-false
+// false
 
-console.log('123' < 57);
+// console.log('123' < 57);
 
-1143
-console.log (5 + 6 + '4' + 9 - 4 - 2);
+// 1143
+// console.log (5 + 6 + '4' + 9 - 4 - 2);
+
+// -------------------------------------------EJERCICIO 8------------------------------------------------------------------
+
+
+// LECTURE: Equality Operators: == vs. ===
+// 1. Declare a variable 'numNeighbours' based on a prompt input like this:
+// prompt('How many neighbour countries does your country
+// have?');
+// 2. If there is only 1 neighbour, log to the console 'Only 1 border!' (use loose equality
+// == for now)
+// 3. Use an else-if block to log 'More than 1 border' in case 'numNeighbours'
+// is greater than 1
+// 4. Use an else block to log 'No borders' (this block will be executed when
+// 'numNeighbours' is 0 or any other value)
+// 5. Test the code with different values of 'numNeighbours', including 1 and 0.
+// 6. Change == to ===, and test the code again, with the same values of
+// 'numNeighbours'. Notice what happens when there is exactly 1 border! Why
+// is this happening?
+// 7. Finally, convert 'numNeighbours' to a number, and watch what happens now
+// when you input 1
+// 8. Reflect on
+
+// const numNeighbours = Number(prompt("How many neighbour countries does your conutry have?"));
+ // if(numNeighbours == 1)console.log("Only 1 border!");
+
+// if(numNeighbours === 1){
+//     console.log("Only 1 border!");
+// }else if(numNeighbours > 1){
+// console.log("More than 1 border");
+// }else{
+//     console.log("No borders");
+// }
